aca181fa73f97fd11e9e17aecf868719ab7a7a52
Alaska01 <aadaniel108@gmail.com>
2021-09-26 00:54:07 +0100

    Initialize game

--- src/index.js
+++ src/index.js
@@ -1,11 +1,46 @@
-import 'phaser';
+import Phaser from 'phaser';
+import CreditScene from './Scenes/CreditScene';
+import UserNameScene from './Scenes/UserNameScene';
+import BootScene from './Scenes/BootScene';
+import PreloaderScene from './Scenes/PreloaderScene';
+import TitleScene from './Scenes/TitleScene';
+import SceneMain from './Scenes/SceneMain';
+import SceneGameOver from './Scenes/SceneGameOver';
+import LeaderBoard from './Scenes/LeaderBoard';
+import './assets/css/styles.css';
 
-import { SimpleScene } from './scenes/simple-scene';
-
-const gameConfig = {
-  width: 680,
-  height: 400,
-  scene: SimpleScene
+const config = {
+  type: Phaser.WEBGL,
+  width: 600,
+  height: 640,
+  parent: 'content',
+  dom: {
+    createContainer: true,
+  },
+  backgroundColor: 'black',
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { x: 0, y: 0 },
+    },
+  },
+  scene: [
+    BootScene,
+    CreditScene,
+    UserNameScene,
+    PreloaderScene,
+    TitleScene,
+    LeaderBoard,
+    SceneMain,
+    SceneGameOver,
+  ],
+  pixelArt: true,
+  roundPixels: true,
 };
 
-new Phaser.Game(gameConfig);
+window.global = {
+  score: 0,
+  emitter: null,
+};
+
+window.game = new Phaser.Game(config);
