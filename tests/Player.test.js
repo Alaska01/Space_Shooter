b7eb7ccf904665e099fc60256d36dc20d5f0513d
Alaska01 <aadaniel108@gmail.com>
2021-09-26 01:42:56 +0100

    Add Player test

---
+++ tests/Player.test.js
@@ -0,0 +1,10 @@
+const Player = (callback) => {
+    callback();
+  };
+  
+  test('Expect Player to have been called', () => {
+    const mockCallback = jest.fn();
+    Player(mockCallback);
+    expect(mockCallback).toHaveBeenCalledWith();
+  });
+  
