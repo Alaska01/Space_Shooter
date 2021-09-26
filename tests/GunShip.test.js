6cb7a4c91df2c265b661de99abcddd920bd4eced
Alaska01 <aadaniel108@gmail.com>
2021-09-26 01:41:08 +0100

    Test GunShip class

---
+++ tests/GunShip.test.js
@@ -0,0 +1,10 @@
+const GunShip = (callback) => {
+    callback();
+  };
+  
+  test('Expect Gunship to have been called', () => {
+    const mockCallback = jest.fn();
+    GunShip(mockCallback);
+    expect(mockCallback).toHaveBeenCalledWith();
+  });
+  
