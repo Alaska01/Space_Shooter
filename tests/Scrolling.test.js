e64a8670e68b7f0b7cc5666e2fcc36b8ed4e77cd
Alaska01 <aadaniel108@gmail.com>
2021-09-26 01:46:09 +0100

    Add Scrolling test

---
+++ tests/Scrolling.test.js
@@ -0,0 +1,10 @@
+const Scrolling = (callback) => {
+    callback();
+  };
+  
+  test('Expect Scrolling to have been called', () => {
+    const mockCallback = jest.fn();
+    Scrolling(mockCallback);
+    expect(mockCallback).toHaveBeenCalledWith();
+  });
+  
